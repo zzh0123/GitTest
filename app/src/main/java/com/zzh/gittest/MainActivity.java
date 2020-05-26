c30f89089da7f579780dd201012e9213e079a35a
zzh0123 <18311004536@163.com>
2020-05-26 23:37:30 +0800

    testa

--- app/src/main/java/com/zzh/gittest/MainActivity.java
+++ app/src/main/java/com/zzh/gittest/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     // remote branch
+    // testa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
