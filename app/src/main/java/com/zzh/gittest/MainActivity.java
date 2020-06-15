93d6ef076f4a6cf2670d76b9eb93225a417c68fc
zhuzh <zhuzh@izkml.com>
2020-06-15 10:33:45 +0800

    hehe7 commit hehe

--- app/src/main/java/com/zzh/gittest/MainActivity.java
+++ app/src/main/java/com/zzh/gittest/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     // hehe7 commit
     // hehe7 change branch hehe
     // hehe7 change again
+    // hehe7 commit hehe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
