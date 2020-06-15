8cfc254d935e196611bb7e5eb724f2ded08236c5
zhuzh <zhuzh@izkml.com>
2020-06-15 09:27:19 +0800

    hehe1

--- app/src/main/java/com/zzh/gittest/MainActivity.java
+++ app/src/main/java/com/zzh/gittest/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     // 123a
     // branch 21
     // hehe
+    // hehe1
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
