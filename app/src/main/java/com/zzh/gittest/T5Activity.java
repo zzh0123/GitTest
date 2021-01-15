73ebab429942212a375f222960f1d5efb57034ea
zzh <18311004536@163.com>
2021-01-15 09:49:47 +0800

    t5 commit1

---
+++ app/src/main/java/com/zzh/gittest/T5Activity.java
@@ -0,0 +1,14 @@
+package com.zzh.gittest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class T5Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_t5);
+    }
+}
