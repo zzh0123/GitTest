902b7435decd17286c54a91103a36a85c3c97a9e
z zh <18311004536@163.com>
2024-01-10 15:20:38 +0800

    改了发生冲突1

---
+++ app/src/main/java/com/zzh/gittest/Constants.java
@@ -0,0 +1,10 @@
+package com.zzh.gittest;
+
+/**
+ * @Description:
+ * @Author: zzh
+ * @CreateDate: 2024/1/10 15:19
+ */
+public interface Constants {
+    String str = "20";
+}
--- app/src/main/java/com/zzh/gittest/MainActivity.java
+++ app/src/main/java/com/zzh/gittest/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
     // remote branch
@@ -11,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
+        Map<String, String> map = new HashMap<>();
+        map.put("b", Constants.str);
+
         // haha
         // xixi haha
         // ni hao
